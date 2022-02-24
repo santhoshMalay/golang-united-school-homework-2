1b1ec369eb171eb807da90d9bf98994934741be5
Santhosh Malay <santhosh_malay@epam.com>
2022-02-24 20:46:42 +0530

    Initial version of HW2 with Iota

--- solution.go
+++ solution.go
@@ -1,12 +1,29 @@
 package square
 
-// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+import (
+	"math"
+)
 
-// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
-// it's like:
-// CalcSquare(10.0, SidesTriangle)
-// CalcSquare(10.0, SidesSquare)
-// CalcSquare(10.0, SidesCircle)
+type numOfSides uint8
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+const (
+	SidesCircle numOfSides = iota
+	_
+	_
+	SidesTriangle
+	SidesSquare
+)
+
+func CalcSquare(sideLen float64, sidesNum numOfSides) float64 {
+	var area float64 = 0
+	if sidesNum == SidesTriangle {
+		area = ((math.Sqrt(3) / 4) * sideLen * sideLen)
+	} else if sidesNum == SidesSquare {
+		area = (sideLen * sideLen)
+	} else if sidesNum == SidesCircle {
+		area = math.Pi * sideLen * sideLen
+	} else {
+		area = 0
+	}
+	return area
 }
